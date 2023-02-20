54c3d6ae91cf03c0ba10cf2d30329df600f1ed7c
muzairaslam <m.uzair@statdevs.com>
2023-02-20 20:06:12 +0500

    cleaned the data

--- R/custom-line-U.R
+++ R/custom-line-U.R
@@ -1,6 +1,6 @@
 ################################################################################
 # Data Set: FFF6611.csv
-# Purpose: Create a line chart with inverted axis.
+# Purpose: clean data for plotting.
 # Last edited by: Muhammad Uzair 
 # Last edited date: 20-02-2023
 ################################################################################
@@ -42,10 +42,34 @@ here::i_am("R/custom-line-U.R")
 df <- read_csv(here("data","raw-data","FFF6611.csv"))
 
 
-#
+
+# rename columns --------------------------------------------------------------
+
+df <- df |>
+  rename(datetime = "RIGTIME",
+         bit_pos_ft = "Bit Position (ft)",
+         hole_dep_ft = "Hole Depth (ft)",
+         rig_state = "RigState") 
+
+
+# data cleaning ----------------------------------------------------------------
+
+
+# remove rows where bit_pos_ft and hole_dep_ft is NULL
+df <- df |>
+  na.omit()
 
 
 
+# convert datetime as lubridate datetime
+df$datetime <- ymd_hms(df$datetime)
+
+
+# saving the clean data
+
+# save the clean co2 pressure and temp data
+write.csv(x = df, file = 'data/clean-data/data-clean.csv',
+          row.names = FALSE)
 
 
 
